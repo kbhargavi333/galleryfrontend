b41f69a8389f9f747bc05de410e4135f74ec1fb7
Kanugula Bhargavi <94820947+kbhargavi333@users.noreply.github.com>
2023-07-05 08:13:55 +0530

    Update Home.js

--- src/navbar1/Home.js
+++ src/navbar1/Home.js
@@ -60,13 +60,13 @@ export default function App() {
                     <p class="ptag">When you leave a place, and no longer have the daily reminders of the people from that environment, memories begin to fade. It’s just how our brains work 
                     to keep us focused on the present day’s immediate needs. Sometimes we have to leave a place, or a group of cherished friends, and we wish we could take them with us.
                      Preserving those memories helps us do just that. .</p>
-                    <img id='img' src='https://cdn-bcfmk.nitrocdn.com/fYEZdJKjvuXkKgFpeJrbqdViLvHKUQUB/assets/images/optimized/rev-b721f14/wp-content/uploads/2022/05/07-record-2048x1365.jpeg'/>
+                    <img id='img' src='https://previews.123rf.com/images/derausdo/derausdo0702/derausdo070200006/738432-vintage-handwriting-from-old-letter.jpg'/>
                 </div>
                 <h3>Echoes of yesterday </h3>
                 <div className='container1'>
                     <p class="ptag">Important memories are created at events like birthdays, anniversaries, weddings, and graduations. Memories preserved in the form of photos, video or through 
                     another medium can also be shared during these events, too. What better time than when friends and family are all together?.</p>
-                    <img id='img' src='https://cdn-bcfmk.nitrocdn.com/fYEZdJKjvuXkKgFpeJrbqdViLvHKUQUB/assets/images/optimized/rev-b721f14/wp-content/uploads/2022/05/09-stories-2048x1365.jpeg'/>
+                    <img id='img' src='https://cdn.firstcry.com/education/2022/09/21150110/Activities-With-Family-Photo-Albums.jpg'/>
                 </div>
                 <h3>Make your life project</h3>
                 <div className='container1'>
